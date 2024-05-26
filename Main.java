49a50b3992d33bfe915911a141f524be60e57359
Laura <laurafeblesizquierdo@gmail.com>
2024-05-26 16:50:33 +0100

    Añadimos la segunda foto con las dos clases Java

--- Main.java
+++ Main.java
@@ -8,12 +8,12 @@ public static void main(String[] args) {
  double saldoActual;
  cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
  saldoActual = cuenta1.estado();
- System.out.println("Current balance: "+ saldoActual );
+ System.out.println("Current balance : "+ saldoActual );
  
         operaciones(cuenta1);
 
  saldoActual = cuenta1.estado();
- System.out.println("Current balance: "+ saldoActual );
+ System.out.println("Current balance : "+ saldoActual );
  
  }
 
